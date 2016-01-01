e64bebed828c27dd20ec071eab3ed2202653078f
xuyingjie <xuyingjie@outlook.com>
2016-01-01 18:49:15 +0800

    join

---
+++ components/Join.vue
@@ -0,0 +1,66 @@
+<style>
+  .join {
+    width: 100%;
+  }
+</style>
+
+<template>
+  <form class="row join" @submit.prevent="join">
+    <label>Name
+      <input type="text" v-model="name" />
+    </label>
+    <label>Password
+      <input type="text" v-model="secret" />
+    </label>
+    <label>Random Password
+      <input type="text" v-model="user.passwd" />
+    </label>
+    <label>iv
+      <input type="text" v-model="user.iv" />
+    </label>
+    <label>AK
+      <input type="text" v-model="user.AK" />
+    </label>
+    <label>SK
+      <input type="text" v-model="user.SK" />
+    </label>
+    <button type="submit">JOIN</button>
+  </form>
+</template>
+
+<script lang="babel">
+  import {upload} from '../utils/http';
+
+  export default {
+    data() {
+      return {
+        name: '',
+        secret: '',
+
+        user: {
+          AK: '',
+          SK: '',
+          passwd: '',
+          iv: '',
+        },
+      };
+    },
+
+    methods: {
+      join() {
+        upload({
+          key: this.name,
+          data: JSON.stringify({
+            user: this.user,
+          }),
+          passwd: this.secret,
+          success: () => {
+            localStorage.user = JSON.stringify(this.user);
+            location.replace('#');
+          },
+        });
+      },
+    },
+  }
+
+</script>
--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './components/App.vue'
+import Join from './components/Join.vue'
 
-new Vue({
+var app = new Vue({
   el: 'body',
-  components: { App }
+  data: {
+    currentView: 'App'
+  },
+  components: { App, Join }
 });
+
+
+// router
+function hashChange() {
+  let hash = location.hash.split('/');
+  if (hash[1] === 'join') {
+    app.currentView = 'Join';
+  } else {
+    app.currentView = 'App';
+  }
+}
+
+hashChange();
+window.onhashchange = hashChange;
